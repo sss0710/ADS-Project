05c67ae2abbf6b24629e8cc81fcd0bd7b5e54fc8
Saurabhsheshank <sinha.saurabhsheshank@gmail.com>
2022-01-09 17:07:33 +0530

    Main updated

--- src/tester/MainProg.java
+++ src/tester/MainProg.java
@@ -2,45 +2,70 @@ package tester;
 
 import java.util.Scanner;
 
+import app.binarytree.Node;
+import app.exception.InvalidExpressionException;
+import app.utils.ConversionFromInfixToPrefix;
+
 import static app.utils.ValidationUtils.validateInfixExpression;
 
 public class MainProg 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InvalidExpressionException 
 	{
 		try(Scanner sc = new Scanner(System.in))
 		{
 			System.out.println("*****Welcome to the Expression Calculator*****");
-			System.out.println("Select the type of expression you want to evaluate"
-								+ "\n1. Prefix Expression"
-								+ "\n2. Infix Expression"
-								+ "\n3. Postfix Expression"
-								+ "\n4. Exit"
-								+ "\nChoose an option from the above list");
+			ConversionFromInfixToPrefix conversion;
+			
+			System.out.println("Enter valid Infix Expression");
+			String infixExpression = validateInfixExpression(sc.next());
+			
+			conversion = new ConversionFromInfixToPrefix();
+			Node root = conversion.creatingExpressionTreefromInfixExpression("(" + infixExpression.toLowerCase() + ")");
+			String preOrderExpression = conversion.preorder(root);
+			String postOrderExpression = conversion.postorder(root);
 			boolean flag = true;
-			String expn;
+			
 			while(flag)
 			{
+				
+				System.out.println("Select the order in which you wish to convert the expression"
+						+ "\n1. Prefix Expression"
+						+ "\n2. Postfix Expression"
+						+ "\n3. Prefix & Postfix Expression"
+						+ "\n4. Enter a new Infix Expression"
+						+ "\n5. Exit"
+						+ "\nChoose an option from the above list");	
 				try
 				{
 					switch (sc.nextInt()) 
 					{
 						case 1: 
-							System.out.println("Enter valid Prefix Expression");
-							//expn = validatePrefixExpression(sc.next());
+							System.out.println("The prefix expression is : " +preOrderExpression);
 							break;
 							
 						case 2: 
-							System.out.println("Enter valid Infix Expression");
-							expn = validateInfixExpression(sc.next());
+							System.out.println("The postfix expression is : " +postOrderExpression);
 							break;
 							
 						case 3: 
-							System.out.println("Enter valid Postfix Expression");
-							
+							System.out.println("The prefix expression is : " +preOrderExpression);
+							System.out.println("The postfix expression is : " +postOrderExpression);
 							break;
 							
 						case 4:
+							System.out.println("Enter valid Infix Expression");
+							infixExpression = validateInfixExpression(sc.next());
+							conversion = new ConversionFromInfixToPrefix();
+							root = conversion.creatingExpressionTreefromInfixExpression("(" + infixExpression.toLowerCase() + ")");
+							
+							System.out.println("The infix expression is : " +infixExpression);
+							preOrderExpression = conversion.preorder(root);
+							postOrderExpression = conversion.postorder(root);
+							
+							break;
+							
+						case 5:
 							flag = false;
 							System.out.println("Call us anytime, Bye for now!!!!");
 							break;
@@ -52,6 +77,7 @@ public class MainProg
 				}
 				catch (Exception e) {
 					System.out.println(e.getMessage());
+					sc.nextLine();
 				}
 			}
 		}
